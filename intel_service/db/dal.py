082c6ae0715e034024433d3f9b7377bceab68f53
Israel Erlenger <israel.erlanger1@gmail.com>
2026-03-16 18:08:56 +0200

    עדכון מטרה במקום בופת חדשה

--- intel_service/db/dal.py
+++ intel_service/db/dal.py
@@ -4,9 +4,12 @@ def find_target(target):
     entity_id = target['entity_id']
     collection = Mongo.get_collection()
     last_target = collection.find_one({'entity_id': entity_id})
+
+    if last_target: 
+        collection.delete_one({'entity_id': entity_id}) # לא יעיל במקרה של שגיאות 
+
     return last_target
     
-
 
 
 def save_target_to_mongo(target):
@@ -14,3 +17,5 @@ def save_target_to_mongo(target):
     collection.insert_one(target)
     print('target saved')
     print(target)
+
+
--- intel_service/main.py
+++ intel_service/main.py
@@ -8,13 +8,16 @@ from logic.haversine import haversine_km
 # 
 def calculate_distance_and_level(target, last_target):
     if not last_target:
-            target['priority_level'] = 99
-            target['movement_distance'] = 0
+        target['priority_level'] = 99
+        target['movement_distance'] = 0
+        save_target_to_mongo(target)
 
     else:
         lat1, lon1 = target['lat'], target['lon']
         lat2, lon2 = last_target['lat'], last_target['lon']
         target['movement_distance'] = haversine_km(lat1, lon1, lat2, lon2)
+        
+
 
     return target
 
